5d3d86422dc1068e3becaa20466d492c03153e39
Vincus999 <92683593+Vincus999@users.noreply.github.com>
2025-05-29 01:16:17 +0200

    Create RefGenome_indexer.R

---
+++ RefGenome_indexer.R
@@ -0,0 +1,7 @@
+library("Rsubread") 
+ 
+buildindex( 
+  basename = "/home/user/project/0031_RefGenome/Homo_sapiens_index", 
+  reference = "/home/user/project/0031_RefGenome/HS_DATA.fa", 
+  memory = 15000 
+)
